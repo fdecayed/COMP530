7d38b455dae449599ec4966f56304a9da0cba0e7
Deyu Fu <fdecayed@gmail.com>
2016-02-10 11:30:39 -0600

    add A2

---
+++ Assignment02/A2/Main/DatabaseTable/headers/MyDB_TableRecIterator.h
@@ -0,0 +1,11 @@
+#ifndef TABLE_REC_ITER_H
+#define TABLE_REC_ITER_H
+
+using namespace std;
+
+class MyDB_TableRecIter:public virtual MyDB_RecordIterator{
+
+};
+
+
+#endif
---
+++ Assignment02/A2/Main/DatabaseTable/source/MyDB_TableRecIterator.cc
@@ -0,0 +1,10 @@
+#ifndef TABLE_REC_ITER_C
+#define TABLE_REC_ITER_C
+#include "MyDB_TableRecIterator.h"
+
+using namespace std;
+
+
+
+
+
