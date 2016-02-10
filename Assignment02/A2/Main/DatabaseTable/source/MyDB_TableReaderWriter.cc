69317c35c24a9d789379d66ba7da7b9065c1a7f2
Deyu Fu <fdecayed@gmail.com>
2016-02-10 17:30:09 -0600

    tablereader

--- Assignment02/A2/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc
+++ Assignment02/A2/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc
@@ -9,14 +9,13 @@
 using namespace std;
 
 MyDB_TableReaderWriter :: MyDB_TableReaderWriter (MyDB_TablePtr tp, MyDB_BufferManagerPtr bp) {
-	buffermgrPtr = bp;
-	tablePtr = tp;
-
+  buffermgrPtr = bp;
+  tablePtr = tp;
+  for(int i=0;i<=tp->lastPage();++i) pageRWs.push_back(MyDB_PageReaderWriter(bp->getPage(tp,i),bp));
 }
 
-MyDB_PageReaderWriter &MyDB_TableReaderWriter :: operator [] (size_t) {
-	static MyDB_PageReaderWriter temp;
-	return temp;	
+MyDB_PageReaderWriter &MyDB_TableReaderWriter :: operator [] (size_t i) {
+  return pageRWs[i];	
 }
 
 MyDB_RecordPtr MyDB_TableReaderWriter :: getEmptyRecord () {
@@ -25,19 +24,33 @@ MyDB_RecordPtr MyDB_TableReaderWriter :: getEmptyRecord () {
 }
 
 MyDB_PageReaderWriter &MyDB_TableReaderWriter :: last () {
-	return (*this)[0];
+  return pageRWs.back();
 }
 
 
-void MyDB_TableReaderWriter :: append (MyDB_RecordPtr) {
+void MyDB_TableReaderWriter :: append (MyDB_RecordPtr record) {
+  if(last().append(record)) return;
+  MyDB_PageReaderWriter temp(buffermgrPtr->getPage(tablePtr,tablePtr->lastPage()+1),buffermgrPtr);
+  temp.append(record);
+  pageRWs.push_back(temp);
 }
 
-void MyDB_TableReaderWriter :: loadFromTextFile (string) {
+void MyDB_TableReaderWriter :: loadFromTextFile (string fromMe) {
+  tablePtr->setLastPage(-1);
+  pageRWs.clear();
+  MyDB_RecordPtr record = getEmptyRecord();
+  ifstream myfile(fromMe);
+  string line;
+  if(myfile.is_open()){
+    while(getline(myfile,line)){
+      record->fromString(line);
+      append(record);
+    }
+  }
 }
 
-MyDB_RecordIteratorPtr MyDB_TableReaderWriter :: getIterator (MyDB_RecordPtr) {
-shared_from_this();
-	return nullptr;
+MyDB_RecordIteratorPtr MyDB_TableReaderWriter :: getIterator (MyDB_RecordPtr record) {
+  return make_shared<MyDB_RecordIterator>(shared_from_this(),record);
 }
 
 void MyDB_TableReaderWriter :: writeIntoTextFile (string) {
